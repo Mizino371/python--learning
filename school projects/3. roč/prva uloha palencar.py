6f27f30d1e2ff7bad85507576b482f2ce403137a
Mizino371 <123683640+Mizino371@users.noreply.github.com>
2023-10-17 14:50:48 +0200

    ok

---
+++ school projects/3. roč/prva uloha palencar.py
@@ -0,0 +1,22 @@
+import tkinter,random
+c=tkinter.Canvas(width=800, height=600)
+c.pack()
+
+def strecha(suradnice):
+    x=suradnice.x
+    y=suradnice.y
+    c.create_polygon(x,y,x+20,y-15,x+40,y,fill="brown",)
+
+def domek(suradnice):
+    x=suradnice.x
+    y=suradnice.y
+    f = random.choice(("blue", "yellow", "red","purple", "green", "light blue", "dark red"))
+    c.create_rectangle(x-20,y-20,x+20,y+20,fill=f,outline=f)
+
+def meno():
+    c.create_text(745,595,text="Michal Palenčár", font="Helvica 15")
+
+c.bind("<Button-3>",strecha)
+c.bind("<Button-1>",domek)
+c.bind("<KeyPress-M>",meno())
+c.mainloop()
