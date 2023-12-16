93f9bc06506cd90156503804a0c7a9c794ce8efa
Mizino371 <123683640+Mizino371@users.noreply.github.com>
2023-12-16 22:59:11 +0100

    novinka

---
+++ school projects/kružok/uloha 3/turtle start.py
@@ -0,0 +1,34 @@
+import turtle as t
+import random as r
+sirka = r.randint(100,400)
+
+
+y=70
+
+t.forward(sirka)
+t.left(90)
+t.forward(270)
+t.left(90)
+t.forward(sirka)
+t.left(90)
+t.forward(270)
+
+def troj(strana):
+    t.left(150)
+    t.forward(strana)
+    t.right(120)
+    t.forward(strana)
+    t.right(30)
+
+for i in range(22):
+    if t.xcor() <=sirka-80:
+        dlzka_strany_troj=r.randint(20,80)
+        troj(dlzka_strany_troj)
+    elif t.xcor() > sirka-80:
+        dlzka_strany_troj = sirka - t.xcor()
+        troj(dlzka_strany_troj)
+        break
+
+
+
+t.mainloop()
