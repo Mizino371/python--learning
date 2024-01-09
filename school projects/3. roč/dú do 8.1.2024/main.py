42821abce06ec575d646a1b612226bd4dffbf63f
Mizino371 <123683640+Mizino371@users.noreply.github.com>
2024-01-09 22:02:10 +0100

    školna uloha oprava

--- school projects/3. roč/dú do 8.1.2024/main.py
+++ school projects/3. roč/dú do 8.1.2024/main.py
@@ -3,76 +3,40 @@
 # Moj postup je taký, že som si vytvoril funkciu move_ball, 
 # ktorá posúva guľu o 5 px dole. 
 
-# import tkinter as tk
-# c = tk.Canvas( width=400, height=400)
-# c.pack()
-# 
-# def move_ball():
-    # c.move(ball, 0, 5)
-    # c.after(100, move_ball)
-    # 
-# ball = c.create_oval(195, 195, 205, 205, fill='red')
-# 
-# move_ball()
-# 
-# c.mainloop()
-# 
- 
-# 2. potrebný postup
 import tkinter as tk
 c = tk.Canvas( width=400, height=400)
 c.pack()
-x=195
-y = 195
-ball = c.create_oval(x, y, x+10, y+10, fill='red')
+
 def move_ball():
-    global x, y
-   
-    c.after(1,ball)
-    y += 5
-
-def vym_lopty():
-    c.delete(100,ball)
-
-
-
-def loop():
-    move_ball()
+    c.move(ball, 0, 5)
+    c.after(100, move_ball)
     
-    c.after(100,loop,)
+ball = c.create_oval(195, 195, 205, 205, fill='red')
 
-loop()
-
+move_ball()
 
 c.mainloop()
 
-# import tkinter as tk
-# 
-# c = tk.Canvas(width=400, height=400)
-# c.pack()
-# 
-# Globálne premenné pre x, y súradnice a identifikátor loptičky
-# x = 195
-# y = 195
-# ball = None  # Na začiatku nemáme žiadnu loptičku
-# 
-# def move_ball():
-    # global x, y, ball
-# 
-    # Vymažeme starú loptičku
-    # if ball is not None:
-        # c.delete(ball)
-# 
-    # Vytvoríme novú loptičku a posunieme ju dolu
-    # y += 5
-    # ball = c.create_oval(x, y, x+10, y+10, fill='red')
-    # c.after(100, move_ball)
-# 
-# Spustenie prvej iterácie
-# 
-# def loop():
-    # move_ball()
-    # c.after(100, loop)
-# lopp()
-# c.mainloop()
-# 
+ 
+# 2. potrebný postup
+
+
+
+import tkinter
+c= tkinter.Canvas()
+c.pack()
+
+def lopta():
+    global y 
+    c.delete('all')
+    c.create_oval(x-5, y-5, x+5, y+5, fill='red')
+    y=y+5
+    if y<200:
+        c.after(100, lopta)
+        
+x=200
+y=5
+
+lopta()
+
+c.mainloop()
