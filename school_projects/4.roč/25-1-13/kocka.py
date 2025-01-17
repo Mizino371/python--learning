f13bb9a0d06e80762a11e369c2806d4a6fd45448
Mizino371 <123683640+Mizino371@users.noreply.github.com>
2025-01-17 09:08:51 +0100

    nedokoncena uloha

--- school_projects/4.roč/25-1-13/kocka.py
+++ school_projects/4.roč/25-1-13/kocka.py
@@ -1,14 +1,36 @@
-import tkinter
-c = tkinter.Canvas(width=610,height=150)
+import tkinter, random as r 
+sirka_p = 600
+vyska_p = 400
+c = tkinter.Canvas(width=sirka_p,height=vyska_p)
 c.pack()
 strany_kocky_z = []
 
 for i in range(6):
     strany_kocky_z.append(tkinter.PhotoImage(file =f"4.roč/25-1-13/fotka_kocky/kocka_{i+1}.png"))
 
-x = 50
-for i2 in range(6):     
-    c.create_image(x,75,image =strany_kocky_z[i2])
-    x += 102
+def vymaž_všetko():
+    c.delete("all")
+
+def vyhodnotenie():
+    if hod_kociek[0] == hod_kociek[1] ==hod_kociek[2]:
+        c.create_text(sirka_p/2,vyska_p/2,text="Bingo",fill = "Blue")
+
+def hod_3_kockami():
+    global hod_kociek
+    x = (sirka_p/2) - 102
+    hod_kociek = 3* [0]
+    for i2 in range(3):
+        nahodne_cisla = r.randrange(3)
+        c.create_image(x,(vyska_p/2)-(vyska_p/6),image =strany_kocky_z[nahodne_cisla])
+        x += 102
+        hod_kociek[i2] += (nahodne_cisla+1)
+    vyhodnotenie()
+    c.update()
+    c.after(1000,vymaž_všetko())
     
+        
+but_generuj = tkinter.Button(text="Hod kockou", command=hod_3_kockami)       
+but_generuj.pack()
+
+ 
 c.mainloop()
---
+++ school_projects/4.roč/25-1-16/palencar_viz_pocasia.py
@@ -0,0 +1,41 @@
+import tkinter
+c=tkinter.Canvas(width=1000,height=600,background="white")
+c.pack()
+data = {
+    "pocasie": ["jasno","polojasno","polooblacno","oblacno","dazd","snezenie","burka","slovakia"]
+        }
+index_pocasia = [0]
+data["Obrazky"] = [
+    tkinter.PhotoImage(file=f"4.roč/25-1-16/obrazky/{pocasie}.png")
+    for pocasie in data["pocasie"]
+                    ]   
+
+c.create_image(500,260,image= data["Obrazky"][7])
+c.create_text(850,560,text="1. Napíš do okienka dolnú hranicu teploty\n2. Vyber si počasie pomocou kliknutia",fill= "Black")
+
+def pocasie_na_vyber():
+    x = 50
+    for i in range(7):
+        tag = str(data["pocasie"][i].strip())
+        c.create_image(x,560,image = data["Obrazky"][i], tags = (tag,"image"))
+        x+= 100
+    c.tag_bind("image", "<Button-1>", klikacka)
+
+
+def klikacka(sur):
+    clicked_tag = c.gettags("current")
+    if str(clicked_tag[0]) in data["pocasie"]  :
+        index_pocasia[0]= data["pocasie"].index(str(clicked_tag[0]))
+    if sur.y < 520:
+        c.create_image(sur.x, sur.y,image = data["Obrazky"][index_pocasia[0]])
+        c.update()
+    print(index_pocasia)
+   
+            
+c.bind("<Button-1>",klikacka)
+pocasie_na_vyber()
+
+entry = tkinter.Entry()
+entry.pack()
+
+c.mainloop()
